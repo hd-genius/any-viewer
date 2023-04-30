802aaf2424d8b4b4eb03712534f1189b97d84d3e
Roy Stewart <roythomasstewart@gmail.com>
2023-04-30 08:56:41 -0400

    Add initial shader loading logic

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,8 @@
 import { ApplicationError, CompatibilityError } from './errors'
+import vertexShader from "../shaders/vertex/default.glsl"
+import fragmentShader from "../shaders/fragment/default.glsl"
+
+let webGL
 
 window.addEventListener('load', () => {
     try {
@@ -16,7 +20,7 @@ window.addEventListener('load', () => {
 
 function setupWebGL() {
     const modelViewPort = document.getElementById('model-view-port')
-    const webGL = modelViewPort.getContext('webgl')
+    webGL = modelViewPort.getContext('webgl')
 
     if (!webGL) {
         throw new CompatibilityError('must support WebGL')
@@ -24,6 +28,58 @@ function setupWebGL() {
 
     webGL.clearColor(0.0, 0.0, 0.0, 1.0)
     webGL.clear(webGL.COLOR_BUFFER_BIT)
+
+    const vertexShader = loadShader(webGL.VERTEX_SHADER, vertexShader)
+    const fragmentShader = loadShader(webGL.FRAGMENT_SHADER, fragmentShader)
+
+    // Create the shader program
+    const shaderProgram = webGL.createProgram()
+    webGL.attachShader(shaderProgram, vertexShader)
+    webGL.attachShader(shaderProgram, fragmentShader)
+    webGL.linkProgram(shaderProgram)
+
+    // If creating the shader program failed, alert
+
+    if (!webGL.getProgramParameter(shaderProgram, webGL.LINK_STATUS)) {
+        alert(
+            `Unable to initialize the shader program: ${webGL.getProgramInfoLog(
+                shaderProgram
+            )}`
+        )
+        return null
+    }
+
+    return shaderProgram
+}
+
+//
+// creates a shader of the given type, uploads the source and
+// compiles it.
+//
+function loadShader(type, source) {
+    const shader = webGL.createShader(type)
+
+    // Send the source to the shader object
+
+    webGL.shaderSource(shader, source)
+
+    // Compile the shader program
+
+    webGL.compileShader(shader)
+
+    // See if it compiled successfully
+
+    if (!webGL.getShaderParameter(shader, webGL.COMPILE_STATUS)) {
+        alert(
+            `An error occurred compiling the shaders: ${webGL.getShaderInfoLog(
+                shader
+            )}`
+        )
+        webGL.deleteShader(shader)
+        return null
+    }
+
+    return shader
 }
 
 function handleFileChange() {
