6d74411444d90db09eb7582f7c30596b6281399d
Roy Stewart <roythomasstewart@gmail.com>
2023-05-02 11:19:32 -0400

    Increase action button size

--- src/components/action-button/action-button.js
+++ src/components/action-button/action-button.js
@@ -1,10 +1,17 @@
+import { registerComponent } from '../utils'
 import buttonIcon from './button-icon.svg'
 import styles from './action-button.module.css'
 
 // spin x into a + on close
 export class ActionButton extends HTMLButtonElement {
     constructor() {
-        this.innerText = buttonIcon
+        super()
         this.classList.add(styles.actionButton)
+        const icon = document.createElement("img")
+        icon.src = buttonIcon;
+        icon.classList.add(styles.icon)
+        this.appendChild(icon)
     }
 }
+
+registerComponent(ActionButton, 'action-menu-button', {extends: "button"})
