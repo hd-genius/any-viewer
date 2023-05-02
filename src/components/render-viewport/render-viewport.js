d8456df065dcb7f80e30c1c678415f77ca68eaf8
Roy Stewart <roythomasstewart@gmail.com>
2023-05-02 15:56:10 -0400

    Move renderer setup logic to component

--- src/components/render-viewport/render-viewport.js
+++ src/components/render-viewport/render-viewport.js
@@ -5,11 +5,75 @@ export class RenderViewport extends HTMLCanvasElement {
     constructor() {
         super()
     }
+
     connectedCallback() {
         this.width = window.innerWidth
         this.height = window.innerHeight
-        
+
         this.classList.add(styles.modelViewPort)
+
+        this.setupWebGL()
+    }
+
+    get webGL() {
+        return this.getContext('webgl')
+    }
+
+    setupWebGL() {
+        if (!this.webGL) {
+            throw new CompatibilityError('must support WebGL')
+        }
+    
+        this.webGL.clearColor(0.0, 0.0, 0.0, 1.0)
+        this.webGL.clear(this.webGL.COLOR_BUFFER_BIT)
+    
+        const vertexShader = loadShader(this.webGL.VERTEX_SHADER, vertexShader)
+        const fragmentShader = loadShader(this.webGL.FRAGMENT_SHADER, fragmentShader)
+    
+        // Create the shader program
+        const shaderProgram = this.webGL.createProgram()
+        this.webGL.attachShader(shaderProgram, vertexShader)
+        this.webGL.attachShader(shaderProgram, fragmentShader)
+        this.webGL.linkProgram(shaderProgram)
+    
+        // If creating the shader program failed, alert
+    
+        if (!this.webGL.getProgramParameter(shaderProgram, this.webGL.LINK_STATUS)) {
+            alert(
+                `Unable to initialize the shader program: ${this.webGL.getProgramInfoLog(
+                    shaderProgram
+                )}`
+            )
+            return null
+        }
+    
+        return shaderProgram
+    }
+    
+    loadShader(type, source) {
+        const shader = this.webGL.createShader(type)
+    
+        // Send the source to the shader object
+    
+        this.webGL.shaderSource(shader, source)
+    
+        // Compile the shader program
+    
+        this.webGL.compileShader(shader)
+    
+        // See if it compiled successfully
+    
+        if (!this.webGL.getShaderParameter(shader, this.webGL.COMPILE_STATUS)) {
+            alert(
+                `An error occurred compiling the shaders: ${this.webGL.getShaderInfoLog(
+                    shader
+                )}`
+            )
+            this.webGL.deleteShader(shader)
+            return null
+        }
+    
+        return shader
     }
 }
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,80 +2,10 @@ import { ApplicationError, CompatibilityError } from './errors'
 import vertexShader from '../shaders/vertex/default.glsl'
 import fragmentShader from '../shaders/fragment/default.glsl'
 
-let webGL
-
-window.addEventListener('load', () => {
-    try {
-        setupWebGL()
-    } catch (error) {
-        if (error instanceof ApplicationError) {
-            error.handle()
-        } else {
-            throw new ApplicationError()
-        }
+window.addEventListener('error', (error) => {
+    if (error instanceof ApplicationError) {
+        error.handle()
+    } else {
+        throw new ApplicationError()
     }
 })
-
-function setupWebGL() {
-    const modelViewPort = document.getElementById('model-view-port')
-    webGL = modelViewPort.getContext('webgl')
-
-    if (!webGL) {
-        throw new CompatibilityError('must support WebGL')
-    }
-
-    webGL.clearColor(0.0, 0.0, 0.0, 1.0)
-    webGL.clear(webGL.COLOR_BUFFER_BIT)
-
-    const vertexShader = loadShader(webGL.VERTEX_SHADER, vertexShader)
-    const fragmentShader = loadShader(webGL.FRAGMENT_SHADER, fragmentShader)
-
-    // Create the shader program
-    const shaderProgram = webGL.createProgram()
-    webGL.attachShader(shaderProgram, vertexShader)
-    webGL.attachShader(shaderProgram, fragmentShader)
-    webGL.linkProgram(shaderProgram)
-
-    // If creating the shader program failed, alert
-
-    if (!webGL.getProgramParameter(shaderProgram, webGL.LINK_STATUS)) {
-        alert(
-            `Unable to initialize the shader program: ${webGL.getProgramInfoLog(
-                shaderProgram
-            )}`
-        )
-        return null
-    }
-
-    return shaderProgram
-}
-
-//
-// creates a shader of the given type, uploads the source and
-// compiles it.
-//
-function loadShader(type, source) {
-    const shader = webGL.createShader(type)
-
-    // Send the source to the shader object
-
-    webGL.shaderSource(shader, source)
-
-    // Compile the shader program
-
-    webGL.compileShader(shader)
-
-    // See if it compiled successfully
-
-    if (!webGL.getShaderParameter(shader, webGL.COMPILE_STATUS)) {
-        alert(
-            `An error occurred compiling the shaders: ${webGL.getShaderInfoLog(
-                shader
-            )}`
-        )
-        webGL.deleteShader(shader)
-        return null
-    }
-
-    return shader
-}
