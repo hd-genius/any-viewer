14acdee841d33501af5b21c867a5fb7f3c5ec205
Roy Stewart <roythomasstewart@gmail.com>
2023-05-04 14:27:26 -0400

    Add placeholder tests

---
+++ src/components/action-menu/action-menu-button/action-menu-button.test.mjs
@@ -0,0 +1,11 @@
+import { describe, it } from 'node:test'
+
+describe('action-menu-button', () => {
+    it.todo('should open the owning action menu')
+
+    it.todo('should close the owning action menu')
+
+    it.todo('should appear open')
+
+    it.todo('should appear closed')
+})
---
+++ src/components/action-menu/action-menu/action-menu.test.mjs
@@ -0,0 +1,7 @@
+import { describe, it } from 'node:test'
+
+describe('action-menu', () => {
+    it.todo('should add the open attribute when it is opened')
+
+    it.todo('should remove the open attribute when it is closed')
+})
